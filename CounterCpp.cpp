73e26c441e3706e34a3f390862b21d1b6f0b8f66
CrypticMessenger <2020csb1072@iitrpr.ac.in>
2021-11-07 01:00:29 +0530

    Co-authored-by: amithrx <amithrx@users.noreply.github.com>

--- CounterCpp.cpp
+++ CounterCpp.cpp
@@ -7,7 +7,6 @@ using namespace std;
 // TODO: Research about a better alternative of huffman trees and implement it
 //TODO: Make huffman tree
 
-
 struct leaf_node
 {
     char alpha;
@@ -46,6 +45,14 @@ struct leaf_node *make_leaf(char alpha, int freq)
 
 struct leaf_node **heap = (struct leaf_node **)calloc(128, sizeof(struct leaf_node *));
 
+void swap(struct leaf_node **ptr1, struct leaf_node **ptr2)
+{
+    struct leaf_node *temp = *ptr1;
+    temp = (*ptr1);
+    (*ptr1) = (*ptr2);
+    (*ptr2) = temp;
+}
+
 struct leaf_node **sort_heap(struct leaf_node **heap)
 {
     for (int i = 0; i < 127; ++i)
@@ -68,6 +75,7 @@ int main()
 {
 
     int arr[128];
+
     for (int i = 0; i < 128; i++)
     {
         arr[i] = 0;
@@ -85,7 +93,7 @@ int main()
         arr[int(c)]++;
     }
 
-    // for (int i = 0; i < 128; i++)
+    // for (int i = 0; i < 128; i++)ṇ
     // {
     //     cout << i << " " << arr[i] << " " << endl;
     // }
@@ -107,16 +115,44 @@ int main()
             printf("%c: % d\n", heap[i]->alpha, heap[i]->freq);
         }
     }
+    printf("----------------------x---------------------------\n");
     heap = sort_heap(heap);
+    int heap_size = 0;
     for (int i = 0; i < 128; i++)
     {
         if (heap[i] != NULL)
         {
             printf("%c: % d\n", heap[i]->alpha, heap[i]->freq);
+            heap_size++;
         }
     }
-    
+    int temp_size = heap_size;
 
+    while (temp_size > 0)
+    {
+        struct leaf_node *temp1 = make_leaf(heap[0]->alpha, heap[0]->freq);
+        struct leaf_node *temp2 = make_leaf(heap[1]->alpha, heap[1]->freq);
+        //!check if there is a need to make 2 new nodes
+        struct leaf_node *temp3 = make_leaf(' ', heap[0]->freq + heap[1]->freq);
+        temp3->left = temp1;
+        temp3->right = temp2;
+        // free(heap[0]);
+        // free(heap[1]);
+        heap[0] = NULL;
+        heap[1] = NULL;
+        heap[heap_size] = temp3;
+        struct leaf_node *ptr;
+        for(int i=2;i<heap_size+1;++i)
+        {   
+            ptr=heap[i];
+            heap[i]=heap[i-2];
+            heap[i-2]=ptr;
+        }
+        heap_size--;
+        heap = sort_heap(heap);
+        temp_size--;
+    }
+    printf("%d",heap_size);
     fclose(fptr);
 
     return 0;

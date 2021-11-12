f398415be5f247592bf2469f9d83f816dc2ff88a
amithrx <2020csb1070@iitrpr.ac.in>
2021-11-12 19:05:01 +0530

    Co-authored-by: Ankit Sharma <CrypticMessenger@users.noreply.github.com>

--- Ankitduplicate.cpp
+++ Ankitduplicate.cpp
@@ -3,15 +3,51 @@
 using namespace std;
 
 //! CONVERT TO C AT LAST
+////  Struct start
 // TODO: Research about a better alternative of huffman trees and implement it
+////T Make huffman tree
 
-void print_codings(char characters[], string codings[], int len)
+struct encoding
 {
-    for (int i = 0; i < len; i++)
-    {
-        cout << characters[i] << " : " << codings[i] << endl;
+    char alpha;
+    // string code;
+    int *code;
+};
+
+struct encoding *make_encoding(char alpha, int arr[], int n)
+{
+    struct encoding *new_node = (struct encoding *)malloc(sizeof(struct encoding));
+    // int size = n;
+    new_node->alpha = alpha;
+    //new_node->code = (int *)malloc((n) * sizeof(int));
+    new_node->code = arr;
+    return new_node;
+}
+
+struct encoding **codes = (struct encoding **)malloc(128 * sizeof(struct encoding *));
+
+void print_array(int arr[], int len)
+{
+    for (int i = 0; i < len; i++){
+        cout<<arr[i]<<" ";
     }
-    cout << endl;
+    cout<<endl;
+}
+
+void print_encoding(struct encoding **codes, int total_node)
+{
+    for (int i = 0; i < total_node; i++)
+    {
+        // printf(" %c : %s\n", codes[i]->alpha, codes[i]->code);
+        // cout << codes[i]->alpha << " : " << codes[i]->code << endl;
+        cout << codes[i]->alpha << " : ";
+        int size = sizeof(codes[i]->code) / sizeof(codes[i]->code[0]);
+        for (int j = 0; j < size; ++j)
+        {
+            cout << codes[i]->code[j];
+        }
+        cout << endl;
+    }
 }
 
 struct leaf_node
@@ -19,6 +55,11 @@ struct leaf_node
     char alpha;
     int freq;
     struct leaf_node *left, *right;
+
+    //!
+    //string str;
+
+    //!
 };
 
 struct leaf_node *make_leaf(char alpha, int freq)
@@ -74,39 +115,62 @@ bool is_leaf(struct leaf_node *leaf)
 //! Change names and algorithms.
 int pointer = 0;
 
-void printArr(int arr[], int n, struct leaf_node *root, char characters[], string codings[])
+void printArr(int arr[], int n, struct leaf_node *root,char characters[],string codings[] )
 {
     int i;
     string str = "";
+    int codde[n];
 
     for (i = 0; i < n; ++i)
     {
+        // printf("%d", arr[i]);
         cout << arr[i];
-        str = str + (char)(arr[i] + 48);
+        // str += std::to_string(arr[i]);
+        str=str+(char)(arr[i]+48);
+        codde[i] = arr[i];
     }
     cout << endl;
-    characters[pointer] = root->alpha;
-    codings[pointer] = str;
+    for (i = 0; i < n; ++i)
+    {
+        // printf("%d", arr[i]);
+        cout << codde[i] << " ";
+    }
+    cout << endl;
+    // struct encoding *temp;
+    // temp = make_encoding(root->alpha, codde, n);
+    struct encoding *temp = (struct encoding *)malloc(sizeof(struct encoding));
+    temp->alpha = root->alpha;
+    temp->code = codde;
+    codes[pointer] = temp;
+    characters[pointer]=root->alpha;
+    codings[pointer]=str;
     ++pointer;
+    cout<<str<<endl;
+
+    cout << endl;
 }
 
-void encoder(struct leaf_node *root, int arr[], int top, char characters[], string codings[])
+void encoder(struct leaf_node *root, int arr[], int top,char characters[],string codings[])
 {
 
     if (root->left != NULL)
     {
         arr[top] = 0;
-        encoder(root->left, arr, top + 1, characters, codings);
+        encoder(root->left, arr, top + 1,characters,codings);
     }
     if (root->right != NULL)
     {
         arr[top] = 1;
-        encoder(root->right, arr, top + 1, characters, codings);
+        encoder(root->right, arr, top + 1,characters,codings);
     }
     if (is_leaf(root))
     {
-        // cout << root->alpha << " : ";
-        printArr(arr, top, root, characters, codings);
+        // printf(" %c : ", root->alpha);
+        cout << root->alpha << " : ";
+        // printf("top : %d ", top);
+        // codes[pointer]=make_encoding(root->alpha,arr,top);
+        printArr(arr, top, root,characters,codings);
+        // cout<<endl;
     }
 }
 
@@ -121,7 +185,7 @@ int main()
     }
     FILE *fptr;
     char c;
-    fptr = fopen("./input1.txt", "r");
+    fptr = fopen("./input.txt", "r");
     if (fptr == NULL)
     {
         cout << "Error! opening file" << endl;
@@ -143,7 +207,7 @@ int main()
             index++;
         }
     }
-
+    
     heap = sort_heap(heap);
     int minm = INT_MAX;
     int heap_size = 0;
@@ -189,23 +253,30 @@ int main()
         temp_size--;
     }
 
-    InorderTrav(heap[0]);
+    // InorderTrav(heap[0]);
+    //!
+
+    //!
+    //printf("%d\n",search('f',heap[0]))
 
     //TODO: write it in analysis how we calculated height of huffman tree
     struct leaf_node *root = heap[0];
 
     total_node = ceil(log2((float)(heap[0]->freq) / minm)) - 1;
-
+    // cout<<total_node<<endl;
     int encry[total_node];
 
     int top = 0;
     char characters[total_node1];
     string codings[total_node1];
-    encoder(root, encry, top, characters, codings);
-    cout << endl;
-    //cout << total_node1<< endl;
-
-    print_codings(characters, codings, total_node1);
+    encoder(root, encry, top,characters,codings);
+    cout << total_node1<< endl;
+    //print_encoding(codes, total_node1);
+    //print_array(codes[2]->code,2);
+    for(int i=0;i<total_node1;++i)
+    {
+        cout<<characters[i]<<" : "<<codings[i]<<endl;
+    }
     fclose(fptr);
 
     return 0;
---
+++ testBench.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    FILE *fptr;
+    char c;
+    fptr = fopen("./input1.txt", "w");
+    int num;
+    cin >> num;
+    while (num--)
+    {
+        int n;
+        char ch;
+        cin >> n;
+        cin >> ch;
+
+        if (fptr == NULL)
+        {
+            cout << "Error! opening file" << endl;
+            exit(1);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            fprintf(fptr, "%c", ch);
+        }
+    }
+
+    return 0;
+}
